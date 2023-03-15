a97a2501778a00afeea0a06d7cd973580a615a86
Expedit ALAGBE <expeditalagbe@gmail.com>
2023-03-15 01:51:52 +0000

    Update add_annee.blade.php

--- resources/views/add_annee.blade.php
+++ resources/views/add_annee.blade.php
@@ -55,7 +55,7 @@
                                     class=" appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                                     id="nom" type="text" placeholder="AAAA-AAAA"
                                     name="nom" required
-                                    pattern="\d{4}\/-d{4}"
+                                    pattern="\d{4}-\d{4}"
                                     title="L'année académique doit être au format 'AAAA-AAAA'"
                                   >
                                 </div>
